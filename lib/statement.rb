0ca350186e1378ef5af1e91f0404f8b7ee1e5416
Muna <mmhussein125@gmail.com>
2018-12-04 17:33:35 +0000

    String interpolation in statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -5,7 +5,7 @@ class Statement
   def view_statement(transaction_history)
     puts 'date || credit || debit || balance'
     transaction_history.reverse_each do |row|
-      puts (row[:date]).to_s + ' || ' + (row[:credit]).to_s + ' || ' "#{row[:debit]}" + ' || ' + (row[:balance]).to_s
+      puts "#{row[:date]} || #{row[:credit]} || #{row[:debit]} || #{row[:balance]}" 
     end
   end
 
