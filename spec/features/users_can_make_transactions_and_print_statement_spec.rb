c41c2db2fe59d729fccf6a48d2c3504bda8b6a2c
Muna <mmhussein125@gmail.com>
2018-12-04 20:26:28 +0000

    Mocks dates in feature test

--- spec/features/users_can_make_transactions_and_print_statement_spec.rb
+++ spec/features/users_can_make_transactions_and_print_statement_spec.rb
@@ -2,11 +2,14 @@ require 'account'
 require 'statement'
 
 describe 'Feature test' do
+
+  let(:date) { Time.now.strftime('%d/%m/%Y') }
+
   it 'returns a statement with all transactions' do
     account = Account.new
     account.credit(1000)
     account.credit(2000)
     account.debit(500)
-    expect(account.print_statement).to eq "date || credit || debit || balance\n04/12/2018 ||  || 500.00 || 2500.00\n04/12/2018 || 2000.00 ||  || 3000.00\n04/12/2018 || 1000.00 ||  || 1000.00"
+    expect(account.print_statement).to eq "date || credit || debit || balance\n#{date} ||  || 500.00 || 2500.00\n#{date} || 2000.00 ||  || 3000.00\n#{date} || 1000.00 ||  || 1000.00"
   end
 end
