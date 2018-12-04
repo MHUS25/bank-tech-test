af04e518e9213fc46f6161944057c8cc9a38a940
Muna <mmhussein125@gmail.com>
2018-12-04 19:09:13 +0000

    Statement can print floats

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,9 +2,20 @@ require_relative './account.rb'
 
 class Statement
   def view_statement(transaction_history)
-    puts 'date || credit || debit || balance'
-    transaction_history.reverse_each do |row|
-      puts "#{row[:date]} || #{row[:credit]} || #{row[:debit]} || #{row[:balance]}"
-    end
+     ['date || credit || debit || balance'] +
+       transaction_history.reverse.map do |item|
+         [
+           item[:date],
+           print_float(item[:credit]),
+           print_float(item[:debit]),
+           print_float(item[:balance])
+         ].join(' || ')
+       end
+  end
+
+private
+
+  def print_float(value)
+    format('%.2f', value) unless value.nil?
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,12 +4,16 @@ describe Statement do
   it 'returns a statement with transaction' do
     transaction_history = [{ date: '04/12/2018', credit: 1000, debit: nil, balance: 1000 }]
     statement = Statement.new
-    expect(statement.view_statement(transaction_history)).to eq [{ date: '04/12/2018', credit: 1000, debit: nil, balance: 1000 }]
+    expect(statement.view_statement(transaction_history)).to eq ["date || credit || debit || balance",
+                                                                "04/12/2018 || 1000.00 ||  || 1000.00"]
   end
 
   it 'returns a statement with multiple transactions' do
     transaction_history = [{ date: '04/12/2018', credit: 1000, debit: nil, balance: 1000 }, { date: '05/12/2018', credit: 2000, debit: nil, balance: 3000 }, { date: '06/12/2018', credit: nil, debit: 1000, balance: 2000 }]
     statement = Statement.new
-    expect(statement.view_statement(transaction_history)).to eq [{ date: '04/12/2018', credit: 1000, debit: nil, balance: 1000 }, { date: '05/12/2018', credit: 2000, debit: nil, balance: 3000 }, { date: '06/12/2018', credit: nil, debit: 1000, balance: 2000 }]
+    expect(statement.view_statement(transaction_history)).to eq ["date || credit || debit || balance",
+                                                                "06/12/2018 ||  || 1000.00 || 2000.00",
+                                                                "05/12/2018 || 2000.00 ||  || 3000.00",
+                                                                "04/12/2018 || 1000.00 ||  || 1000.00"]
   end
 end
