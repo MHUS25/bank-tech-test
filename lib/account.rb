d8dc28ea97eb428027e6a7997ad0280fa10a8a74
Muna <mmhussein125@gmail.com>
2018-12-04 16:31:15 +0000

    Records transaction date

--- lib/account.rb
+++ lib/account.rb
@@ -5,12 +5,18 @@ class Account
     @balance = 0
   end
 
-  def credit(deposit)
+  def credit(deposit, date = transaction_date)
     @balance += deposit
   end
 
-  def debit(withdrawal)
+  def debit(withdrawal, date = transaction_date)
     raise "Insufficient funds" if withdrawal > @balance
     @balance -= withdrawal
+  end
+
+  private
+
+  def transaction_date
+    Time.now.strftime("%d/%m/%Y")
   end
 end
