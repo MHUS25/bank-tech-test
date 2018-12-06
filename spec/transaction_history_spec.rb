3a1d3e66301b24e75b49837cfa25fb89d5e01ec0
Muna <mmhussein125@gmail.com>
2018-12-06 11:40:59 +0000

    Passed failing test, WOOP WOOP!

--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -1,20 +1,15 @@
 require 'transaction_history'
 
 describe TransactionHistory do
-  let(:transaction_class) { double :transaction_class }
   subject { TransactionHistory.new(transaction_class) }
+   let(:transaction_class) { spy('transaction_class') }
 
   it 'returns a list of transactions and starts off empty' do
     expect(subject.transaction_history).to be_empty
   end
 
-  # it 'records a new transaction' do
-  #   expect(transaction_class).to receive(:new).with(deposit: 1000, withdrawal: nil, balance: 1000)
-  #   # transaction = transaction_class
-  #   # allow(transaction).to receive(:transaction_data).and_return(date: @date,
-  #   #                                                             credit: @deposit,
-  #   #                                                             debit: @withdrawal,
-  #   #                                                             balance: @balance)
-  #   subject.record_transaction(deposit: 1000, withdrawal: nil, balance: 1000)
-  # end
+  it 'records a new transaction' do
+    subject.record_transaction(deposit: 1000, withdrawal: nil, balance: 1000)
+    expect(transaction_class).to have_recieved(:new)
+  end
 end
